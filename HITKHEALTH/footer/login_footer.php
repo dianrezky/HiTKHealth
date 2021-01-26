39f0109c8f5776bb36efc543094d571eed2a70c4
Dian R <dianrezky03@gmail.com>
2021-01-26 17:47:28 +0700

    project done

--- HITKHEALTH/footer/login_footer.php
+++
@@ -1,5 +0,0 @@
-<script src="/vendor/jquery/jquery.min.js"></script>
-<script src="/js/main.js"></script>
-</body>
-
-</html>
--- HITKHEALTH/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-//Membuat koneksi database
-$koneksi = mysqli_connect("localhost", "hitkhea1_sg05", "hitkhealth05", "hitkhea1_sg");
-
-// Check connection
-if (mysqli_connect_errno()) {
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}
